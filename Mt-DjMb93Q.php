ac046004c9a319a07d9211c7b4929a91936ceb01
Big Guy <104549927+BigManThonga@users.noreply.github.com>
2024-03-05 10:26:41 -0500

    Add files via upload

---
+++ Mt-DjMb93Q.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>WHO DA FAWQ IS GIGANIGGA - MyTube</title>
+    <link rel="icon" href="MyTube.svg" alt="icon">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <h1>MyTube</h1>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/'">Home</button>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/videos.php'">Videos</button>
+    <h1>‎</h1>
+    <video src="uploads/youtube_lpInSlhAayE_720x1280_h264~2.mp4" controls></video>
+    <h2>WHO DA FAWQ IS GIGANIGGA</h2>
+    <nav>
+        <h3>Upload Date: November 8th, 2023</h3>
+        <h3>Description:</h3>
+        <p></p>
+        <h3>Uploader: GigaNigga</h3>
+    </nav>
+</body>
+</html>
---
+++ Mt-Ow4atnP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Pornography - MyTube</title>
+    <link rel="icon" href="MyTube.svg" alt="icon">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <h1>MyTube</h1>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/'">Home</button>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/videos.php'">Videos</button>
+    <h3> <h3>
+    <video src="uploads/cachedVideo-1.qt" controls></video>
+    <h2>Pornography</h2>
+    <nav>
+        <h3>Upload Date: November 10th, 2023</h3>
+        <h3>Description:</h3>
+        <p>Pornography</p>
+        <h3>Uploader: Spy</h3>
+    </nav>
+</body>
+</html>
