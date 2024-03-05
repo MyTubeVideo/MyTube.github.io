c79d6c16712f567f2c3b4c5ddfe227368ba911b5
Big Guy <104549927+BigManThonga@users.noreply.github.com>
2024-03-05 10:25:21 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>We Couldnt Find Shit - MyTube</title>
+    <link href="fucku.png" alt="icon">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <h1>MyTube</h1>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/'">Home</button>
+    <button onclick="location.href='https://mytube.cryptical-corp.repl.co/videos.php'">Videos</button>
+
+    <script src="script.js"></script>
+
+    <h2>404 - Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist.</p>
+</body>
+
+</html>
